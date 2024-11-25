dc72bccbd7321910ce352719b191375374c14906
Higor Gabriel Lino Silva <higor@localhost.localdomain>
2024-11-25 16:13:41 -0300

    update window

--- trabalho3/views/mainWindow.py
+++ trabalho3/views/mainWindow.py
@@ -5,19 +5,22 @@ from gi.repository import Gtk
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         super().__init__(title="Layout com Gtk.Box", application=app)
+        self.set_title("Biblioteca de Jogos")
+        self.set_default_size(800, 600)
 
         # Criar um container horizontal
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
-        self.set_child(hbox)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.set_child(vbox)
+
 
         # Adicionar botões
         button1 = Gtk.Button(label="Botão 1")
         button1.connect("clicked", self.on_button1_clicked)
-        hbox.append(button1)
+        vbox.append(button1)
 
         button2 = Gtk.Button(label="Botão 2")
         button2.connect("clicked", self.on_button2_clicked)
-        hbox.append(button2)
+        vbox.append(button2)
 
     def on_button1_clicked(self, button):
         print("Botão 1 clicado!")
