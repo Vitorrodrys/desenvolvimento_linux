2f486195684fc0c63ed24472aaeb9f1403d9c3de
Higor Gabriel Lino Silva <higor@localhost.localdomain>
2024-12-09 16:53:31 -0300

    add interface

--- trabalho6/mainWindow.py
+++ trabalho6/mainWindow.py
@@ -1,27 +1,20 @@
 import gi
+from board import Board
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gdk
 
 
-class GridApp(Gtk.Application):
-    def __init__(self):
-        super().__init__()
-
-    def do_activate(self):
-        # Configura a janela principal
-        window = Gtk.ApplicationWindow(application=self)
-        window.set_title("Grid 16x16")
-        window.set_default_size(400, 400)
-        # window.set_margin_top(10)
-        # window.set_margin_bottom(10)
-        # window.set_margin_start(10)
-        # window.set_margin_end(10)
+class GridApp(Gtk.ApplicationWindow):
+    def __init__(self, app):
+        super().__init__(application=app, title="Jogo da Vida")  # Definindo o título aqui
+        self.set_default_size(400, 400)
+        self.board = Board(16, 16)
 
         # Criação do grid
         grid = Gtk.Grid()
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        window.set_child(vbox)
+        self.set_child(vbox)
         vbox.append(grid)
 
         # Loop para criar o grid 16x16
@@ -29,30 +22,47 @@ class GridApp(Gtk.Application):
             for col in range(16):
                 button = Gtk.Button()
                 button.add_css_class("grid-button")  # Classe CSS para o botão
+                button.row = row  # Adiciona a linha como atributo
+                button.col = col  # Adiciona a coluna como atributo
                 button.connect("clicked", self.on_button_clicked)
                 grid.attach(button, col, row, 1, 1)
 
+        # Adicionando o botão "Gerar"
         hboxBotao = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         vbox.append(hboxBotao)
-
         hboxBotao.set_margin_top(10)
 
         buttonGerar = Gtk.Button(label="Gerar")
         buttonGerar.connect("clicked", self.on_buttonGerar_clicked)
         hboxBotao.append(buttonGerar)
 
-        # Exibe a janela
-        window.show()
+        self.grid = grid
 
     def on_button_clicked(self, button):
-        # Alterna entre cores para indicar seleção
+        row = button.row
+        col = button.col
+
+        self.board.swap_value(row, col)
+
         if "selected" in button.get_css_classes():
             button.remove_css_class("selected")
         else:
             button.add_css_class("selected")
 
     def on_buttonGerar_clicked(self, button):
-        pass
+        self.board.step()
+        matriz = self.board.get_board_matrix()
+
+        grid = self.grid
+        for row in range(16):
+            for col in range(16):
+                button = grid.get_child_at(col, row)  # Recupera o botão específico da célula
+                if matriz[row][col]:
+                    if "selected" not in button.get_css_classes():
+                        button.add_css_class("selected")  # Adiciona a classe "selected" se o valor for True
+                else:
+                    if "selected" in button.get_css_classes():
+                        button.remove_css_class("selected")  # Remove a classe "selected" se o valor for False
 
 
 # Estilo CSS para controlar a cor
@@ -78,5 +88,13 @@ Gtk.StyleContext.add_provider_for_display(
 )
 
 # Inicializa a aplicação
-app = GridApp()
+class MyApp(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.example.gridapp")
+
+    def do_activate(self):
+        window = GridApp(self)  # Passando a instância da aplicação
+        window.present()
+
+app = MyApp()
 app.run()
