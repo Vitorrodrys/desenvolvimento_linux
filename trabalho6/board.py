efe683d1cfbbb9dd3f944171571e92e4b14018d2
Vitorrodrys <Vitorrodrys@gmail.com>
2024-12-09 16:06:02 -0300

    run ruff formatter

--- trabalho6/board.py
+++ trabalho6/board.py
@@ -1,79 +1,75 @@
-
-
-
 class Board:
-
-    def left(self, i:int, j:int) -> bool:
-        j = j-1
+    def left(self, i: int, j: int) -> bool:
+        j = j - 1
         if j < 0:
             return False
         return self.__board[i][j]
 
-    def rigth(self, i:int, j:int) -> bool:
-        if j+1 >= len(self.__board[i]):
+    def rigth(self, i: int, j: int) -> bool:
+        if j + 1 >= len(self.__board[i]):
             return False
-        return self.__board[i][j+1]
+        return self.__board[i][j + 1]
 
-    def up(self, i:int, j:int) -> bool:
-        i = i-1
+    def up(self, i: int, j: int) -> bool:
+        i = i - 1
         if i < 0:
             return False
         return self.__board[i][j]
 
-    def down(self, i:int, j:int) -> bool:
-        if i+1 >= len(self.__board):
+    def down(self, i: int, j: int) -> bool:
+        if i + 1 >= len(self.__board):
             return False
-        return self.__board[i+1][j]
+        return self.__board[i + 1][j]
 
-    def up_left(self, i:int, j:int ) -> bool:
-        i = i-1
-        j = j-1
+    def up_left(self, i: int, j: int) -> bool:
+        i = i - 1
+        j = j - 1
         if i < 0 or j < 0:
             return False
         return self.__board[i][j]
 
-    def up_rigth(self, i:int, j:int) -> bool:
-        i = i-1
-        if i < 0 or j+1 >= len(self.__board[i]):
+    def up_rigth(self, i: int, j: int) -> bool:
+        i = i - 1
+        if i < 0 or j + 1 >= len(self.__board[i]):
             return False
-        return self.__board[i][j+1]
+        return self.__board[i][j + 1]
 
-    def down_left(self, i:int, j:int) -> bool:
-        j = j-1
-        if j < 0 or i+1 >= len(self.__board):
+    def down_left(self, i: int, j: int) -> bool:
+        j = j - 1
+        if j < 0 or i + 1 >= len(self.__board):
             return False
-        return self.__board[i+1][j]
+        return self.__board[i + 1][j]
 
-    def down_right(self, i:int, j:int) -> bool:
-        if i+1 >= len(self.__board) or j+1 >= len(self.__board[i]):
+    def down_right(self, i: int, j: int) -> bool:
+        if i + 1 >= len(self.__board) or j + 1 >= len(self.__board[i]):
             return False
-        return self.__board[i+1][j+1]
+        return self.__board[i + 1][j + 1]
 
-    def get_neighbours(self, i:int, j:int) -> list[bool]:
+    def get_neighbours(self, i: int, j: int) -> list[bool]:
         return [
-            self.left(i,j),
-            self.rigth(i,j),
-            self.up(i,j),
-            self.down(i,j),
-            self.up_left(i,j),
-            self.up_rigth(i,j),
-            self.down_left(i,j),
-            self.down_right(i,j),
+            self.left(i, j),
+            self.rigth(i, j),
+            self.up(i, j),
+            self.down(i, j),
+            self.up_left(i, j),
+            self.up_rigth(i, j),
+            self.down_left(i, j),
+            self.down_right(i, j),
         ]
-        
+
     def __init__(
         self,
         rows: int,
         columns: int,
     ) -> None:
-        self.__board = [[False]*columns]*rows
+        self.__board = [[False] * columns] * rows
         self.__rows = rows
         self.__columns = columns
 
-    def swap_value(self, i:int, j:int):
+    def swap_value(self, i: int, j: int):
         self.__board[i][j] = not self.__board[i][j]
 
-    def check(self)->tuple[list[tuple[int, int]], list[tuple[int, int]]]:
+    def check(self) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
         relife_list = []
         kill_list = []
         for i in range(self.__rows):
@@ -81,22 +77,22 @@ class Board:
                 neighbors = self.get_neighbours(i, j)
                 sum_neighbours = sum(neighbors)
                 if sum_neighbours < 2:
-                    kill_list.append((i,j))
+                    kill_list.append((i, j))
                 elif sum_neighbours > 3:
-                    kill_list.append((i,j))
+                    kill_list.append((i, j))
                 elif sum_neighbours == 3:
-                    relife_list.append((i,j))
+                    relife_list.append((i, j))
                 elif sum_neighbours == 2:
                     pass
-                    #keep
+                    # keep
         return relife_list, kill_list
 
     def step(self):
         relife_list, kill_list = self.check()
-        for i,j in relife_list:
-            self.swap_value(i,j)
-        for i,j in kill_list:
-            self.swap_value(i,j)
+        for i, j in relife_list:
+            self.swap_value(i, j)
+        for i, j in kill_list:
+            self.swap_value(i, j)
 
     def get_board_matrix(self):
         return self.__board
--- trabalho6/mainWindow.py
+++ trabalho6/mainWindow.py
@@ -1,6 +1,8 @@
 import gi
+
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gdk
+
 
 class GridApp(Gtk.Application):
     def __init__(self):
